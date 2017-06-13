2340c45ca3c5deda5ea54ab76759ee53fa6ddb11
__orange <freepascal@vip.qq.com>
2017-06-14 05:59:18 +0800

    Initial commit.

---
+++ modinfo.lua
@@ -0,0 +1,18 @@
+name = "Yet Another Hunger Games"
+description = "Yet Another Hunger Games"
+
+author = "__orange"
+version = "0"
+forumthread = ""
+api_version = 10
+
+-- icon_atlas = "modicon.xml"
+-- icon = "modicon.tex"
+all_clients_require_mod = false
+client_only_mod = false
+dst_compatible = true
+server_filter_tags = {}
+
+configuration_options =
+{
+}
---
+++ scripts/components/distpenalty.lua
@@ -0,0 +1,47 @@
+local function OnTaskTick(inst, self, period)
+    self:DoDec(period)
+end
+
+local Distpenalty = Class(function(self, inst)
+    self.inst = inst
+
+    self.day = TheWorld.state.cycles
+    self.mp = c_find("multiplayer_portal")
+    self.maxdist = TheWorld.Map:GetSize() * 4
+    self.mindist = 0
+    self.maxdays = 3
+    self.mindays = 0
+
+    local period = 10
+    self.inst:DoPeriodicTask(period, OnTaskTick, nil, self, period)
+end,
+nil,
+{
+})
+
+function Distpenalty:GetPenaltydist()
+    local day = TheWorld.state.cycles
+    if day <= self.mindays then
+        day = 0
+    end
+    local penaltydist = Lerp(self.maxdist, self.mindist, day/self.maxdays)
+    return penaltydist
+end
+
+function Distpenalty:LongUpdate(dt)
+    self:DoDec(dt, true)
+end
+
+function Distpenalty:DoDec(dt)
+    local day = TheWorld.state.cycles
+    if day <= self.mindays then
+        day = 0
+    end
+    local penaltydist = Lerp(self.maxdist, self.mindist, day/self.maxdays)
+    if not self.inst:IsNear(self.mp, penaltydist) then
+        self.inst.components.talker:Say("Too far from Portal!")
+        self.inst.components.health:DoDelta(-dt, true, "distpenalty")
+    end
+end
+
+return Distpenalty
